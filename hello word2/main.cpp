10005ecebd22ea1238e8d122819bb7f0db3469e2
20171105145 <20171105145du@admindeiMac-62.local>
2018-06-26 14:04:13 +0800

    Initial Commit

---
+++ hello word2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello word2
+//
+//  Created by 杜炎峰 on 2018/6/26.
+//  Copyright © 2018年 杜炎峰. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
